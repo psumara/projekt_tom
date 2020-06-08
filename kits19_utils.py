05e7ad301d01f14def8140bcdd7e5799f5dfe50e
kwasniakk <64785433+kwasniakk@users.noreply.github.com>
2020-06-08 16:06:44 +0200

    Update kits19_utils.py

--- kits19_utils.py
+++ kits19_utils.py
@@ -12,39 +12,39 @@ def load_image(filename):
     return img_numpy
     
 def generate_center_plot_slices(image):
-    #Slice_0 - transverse plane
-    #Slice_1 - coronal plane
-    #slice_2 - sagittal plane
-    slice_0 = image[:, image.shape[1] / 2, :]
-    slice_1 = image[:, :, image.shape[2] / 2]
-    slice_2 = image[image.shape[0] / 2, :, :]
+    #Slice_0 - coronal plane
+    #Slice_1 - sagittal plane
+    #slice_2 - transverse plane
+    slice_0 = image[:, int(image.shape[1] / 2), :]
+    slice_1 = image[:, :, int(image.shape[2] / 2)]
+    slice_2 = image[int(image.shape[0] / 2), :, :]
     return slice_0, slice_1, slice_2
 
 def generate_axis_view(image):
     #plot image in three planes
-    fig, ax = plt.subplots(3, sharex = True, figsize = (15, 18))
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex = True, figsize = (9, 6))
     fig.suptitle("Center slices for NII Image")
-    transverse, coronal, sagittal  = generate_center_plot_slices(image)
+    coronal, sagittal, transverse = generate_center_plot_slices(image)
     
-    ax[0].imshow(transverse.T, cmap = "gray", origin = "lower")
-    ax[0].set(title = "Transverse plane")
-    plt.axis("off")
+    ax1.imshow(transverse.T, cmap = "gray", origin = "lower")
+    ax1.set(title = "Transverse plane")
+    ax1.axis("off")
     
-    ax[1].imshow(coronal.T, cmap = "gray", origin = "lower")
-    ax[1].set(title = "Coronal plane")
-    plt.axis("off")
+    ax2.imshow(coronal.T, cmap = "gray", origin = "lower")
+    ax2.set(title = "Coronal plane")
+    ax2.axis("off")
     
-    ax[2].imshow(sagittal.T, cmap = "gray", origin = "lower")
-    ax[2].set(title = "Sagittal plane")
-    plt.axis("off")
+    ax3.imshow(sagittal.T, cmap = "gray", origin = "lower")
+    ax3.set(title = "Sagittal plane")
+    ax3.axis("off")
     
 def get_slices(image, mode = "Transverse"):
     #get image slices in desired plane
-    if (mode == "Transverse"):
+    if (mode == "Coronal"):
         image_slices = [image[:, i, :] for i in range(image.shape[1])]
-    elif (mode == "Coronal"):
-        image_slices = [image[:, :, i] for i in range(image.shape[2])]
     elif (mode == "Sagittal"):
+        image_slices = [image[:, :, i] for i in range(image.shape[2])]
+    elif (mode == "Transverse"):
         image_slices = [image[i, :, :] for i in range(image.shape[0])]
     return image_slices
 
